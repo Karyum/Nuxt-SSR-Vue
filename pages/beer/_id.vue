887196b0b01f83479aba55582b9ac548809fe0bf
mario <mario.saliba98@gmail.com>
2018-01-10 16:00:54 +0200

    create a route for beer /beer/:id

---
+++ pages/beer/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <div v-if="name">
+      <h1>{{ name }}</h1>
+      <img :src="img" alt="">
+      <!-- <code><pre>{{params}}</pre></code> -->
+  </div>
+</template>
+
+<script>
+import axios from '../../plugins/axios';
+
+export default {
+  async asyncData({ params, error }) {
+    try {
+      const { data } = await axios.get(`/api/beer/${params.id}`);
+      return {
+        name: data.name,
+        img: data.img,
+      };
+    } catch (err) {}
+  },
+};
+</script>
+
+<style scoped>
+
+img {
+  width: 100px;
+  height: 100px;
+}
+
+</style>
