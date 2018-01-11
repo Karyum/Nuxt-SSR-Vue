8dfa73ea99d0efeaba1d28a4edd9c244b557e11a
mario <mario.saliba98@gmail.com>
2018-01-11 17:12:10 +0200

    adds an error page with a message rel #1

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1> Mate {{ error.message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'default',
+};
+</script>
+
+<style scoped>
+
+</style>
